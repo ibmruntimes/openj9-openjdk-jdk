2253b94dd84239b352838c0b0a5a72acb0f87286
Babneet Singh <sbabneet@ca.ibm.com>
2020-06-12 13:06:50 -0700

    Revert "8246152: Improve String concat bootstrapping"
    This reverts commit 8efc7e5691f01b83883b95c04b270191bfedbf0c.

--- test/jdk/java/lang/String/concat/StringConcatFactoryInvariants.java
+++ test/jdk/java/lang/String/concat/StringConcatFactoryInvariants.java
@@ -28,7 +28,6 @@ import java.util.concurrent.Callable;
 /**
  * @test
  * @summary Test input invariants for StringConcatFactory
- * @bug 8246152
  *
  * @compile StringConcatFactoryInvariants.java
  *
@@ -214,14 +213,8 @@ public class StringConcatFactoryInvariants {
         ok("Static arguments and recipe match",
                 () -> StringConcatFactory.makeConcatWithConstants(lookup, methodName, mtThreshold, recipeThreshold, "bar"));
 
-        fail("Static arguments and recipe mismatch: too few",
-                () -> StringConcatFactory.makeConcatWithConstants(lookup, methodName, mtThreshold, recipeThreshold));
-
-        fail("Static arguments and recipe mismatch: too many",
+        fail("Static arguments and recipe mismatch",
                 () -> StringConcatFactory.makeConcatWithConstants(lookup, methodName, mtThreshold, recipeThreshold, "bar", "baz"));
-
-        fail("Static arguments and recipe mismatch, too many, overflowing constant is null",
-                () -> StringConcatFactory.makeConcatWithConstants(lookup, methodName, mtThreshold, recipeThreshold, "bar", null));
 
         // Advanced factory: check for mismatched recipe and dynamic arguments
         fail("Dynamic arguments and recipe mismatch",
