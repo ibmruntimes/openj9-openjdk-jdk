dbb696a58f54082f2bc65ee707fa8b718e6c0c30
Gengchen Tuo <gengchen.tuo@ibm.com>
2025-04-09 11:32:52 -0400

    Add @JvmtiMountTransition to VThread mount() and unmount()
    Hide JVMTI events of VirtualThread mount() and unmount() during virtual
    thread mount state transitions. This matches reference implementation's
    behaviour.
    
    Issue: https://github.com/eclipse-openj9/openj9/issues/21399
    
    Co-authored-by: Babneet Singh <sbabneet@ca.ibm.com>
    Signed-off-by: Gengchen Tuo <gengchen.tuo@ibm.com>

--- src/java.base/share/classes/java/lang/VirtualThread.java
+++ src/java.base/share/classes/java/lang/VirtualThread.java
@@ -24,7 +24,7 @@
  */
 /*
  * ===========================================================================
- * (c) Copyright IBM Corp. 2022, 2024 All Rights Reserved
+ * (c) Copyright IBM Corp. 2022, 2025 All Rights Reserved
  * ===========================================================================
  */
 package java.lang;
@@ -490,6 +490,7 @@ final class VirtualThread extends BaseVirtualThread {
      * return, the current thread is the virtual thread.
      */
     @ChangesCurrentThread
+    @JvmtiMountTransition
     @ReservedStackAccess
     private void mount() {
         // notify JVMTI before mount
@@ -520,6 +521,7 @@ final class VirtualThread extends BaseVirtualThread {
      * current thread is the current platform thread.
      */
     @ChangesCurrentThread
+    @JvmtiMountTransition
     @ReservedStackAccess
     private void unmount() {
         assert !Thread.holdsLock(interruptLock);
