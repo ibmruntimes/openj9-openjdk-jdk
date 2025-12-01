87ddc2680990b68f1cf89bac48bff10ee5f48cf2
Dev Agarwal <dev.agarwal@ibm.com>
2025-12-01 09:30:02 -0500

    Update Cipher GetInstance test
    Ensure SunJCE is compared with itself and not
    the first provider in the list supporting PBECipher.
    
    Signed-off-by: Dev Agarwal <dev.agarwal@ibm.com>

--- test/jdk/javax/crypto/Cipher/TestGetInstance.java
+++ test/jdk/javax/crypto/Cipher/TestGetInstance.java
@@ -22,6 +22,12 @@
  */
 
 /*
+ * ===========================================================================
+ * (c) Copyright IBM Corp. 2025, 2025 All Rights Reserved
+ * ===========================================================================
+ */
+
+/*
  * @test
  * @bug 4898428 8368984
  * @summary test that the new getInstance() implementation works correctly
@@ -55,7 +61,8 @@ public class TestGetInstance {
 
         Cipher c;
 
-        c = Cipher.getInstance(pbeAlgo);
+        c = Cipher.getInstance(pbeAlgo,
+                System.getProperty("test.provider.name", "SunJCE"));
         same(p, c.getProvider());
 
         c = Cipher.getInstance(algoLC,
